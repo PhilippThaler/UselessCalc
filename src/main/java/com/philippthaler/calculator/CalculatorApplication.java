5784fd5125d3319305ee92ce976abe309b420bc4
Philipp Thaler <thalerphilipp1@gmail.com>
2018-12-19 14:02:12 +0100

    Updated Text

--- src/main/java/com/philippthaler/calculator/CalculatorApplication.java
+++ src/main/java/com/philippthaler/calculator/CalculatorApplication.java
@@ -14,9 +14,15 @@ public class CalculatorApplication implements CommandLineRunner {
   }
 
   public void run(String... args) throws Exception {
+
+    System.out.println("     _        _   _                _                         ____           _                  _           _                    _   _ _ \n" +
+        "    / \\      | | | |  ___    ___  | |   ___   ___   ___     / ___|   __ _  | |   ___   _   _  | |   __ _  | |_    ___    _ __  | | ( | )\n" +
+        "   / _ \\     | | | | / __|  / _ \\ | |  / _ \\ / __| / __|   | |      / _` | | |  / __| | | | | | |  / _` | | __|  / _ \\  | '__| | |  V V \n" +
+        "  / ___ \\    | |_| | \\__ \\ |  __/ | | |  __/ \\__ \\ \\__ \\   | |___  | (_| | | | | (__  | |_| | | | | (_| | | |_  | (_) | | |    |_|      \n" +
+        " /_/   \\_\\    \\___/  |___/  \\___| |_|  \\___| |___/ |___/    \\____|  \\__,_| |_|  \\___|  \\__,_| |_|  \\__,_|  \\__|  \\___/  |_|    (_)      \n" +
+        "                                                                                                                                        \n");
     Scanner scanner = new Scanner(System.in);
     int num1, num2;
-    System.out.println("A Useless Calculator!\n\n");
     System.out.println("Dieses Programm addiert 2 Zahlen");
     System.out.println("Zahl 1: ");
     num1 = scanner.nextInt();
